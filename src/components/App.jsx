8f082dd0d9dc20dd08316107fa664ac33542897d
Ddemkowicz <dakucp@gmail.com>
2023-06-04 15:27:01 +0200

    Create Home.jsx
    Stworzony zarys szkieletu strony głównej.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,45 @@
+import { useEffect, useState } from 'react';
+import { api } from 'service/api';
+import Home from './Home/Home';
+
 export const App = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    api.fetchGetTrending().then(res => {
+      setMovies(res);
+    });
+  }, []);
+
+  const fetchLog = () => {
+    console.log(movies);
+    // try {
+    //   const movies = await api.fetchSearchMovies('batman');
+    //   console.log(movies);
+    // } catch (error) {
+    //   alert(error);
+    // } finally {
+    //   console.log('finally');
+    // }
+  };
+
   return (
     <div
       style={{
         height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
+      <button type="button" onClick={fetchLog}>
+        LOG
+      </button>
       React homework template
+      <Home movies={movies} />
     </div>
   );
 };
